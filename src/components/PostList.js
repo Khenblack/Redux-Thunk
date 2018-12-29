b566d570565a914c6c971ede35ce257f12aad43c
Ernest Roca Vivó <khenblack@gmail.com>
2018-12-29 14:12:38 +0100

    fetch user

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,19 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
-  render() {
-    return <div>Post List</div>;
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className='item' key={post.id}>
+          <i className='large middle aligned icon user' />
+          <div className='content'>
+            <div className='description'>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId} />
+          </div>
+        </div>
+      );
+    });
   }
+
+  render() {
+    return <div className='ui relaxed divided list'>{this.renderList()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { posts: state.posts };
 }
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({ fetchPosts }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(PostList);
+export default connect(mapStateToProps, mapDispatchToProps)(PostList);
