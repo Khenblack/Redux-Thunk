d228cc2792ec7b872e28be45f5a3f0d9a9f347ed
Ernest Roca Vivó <khenblack@gmail.com>
2018-12-30 10:39:13 +0100

    Memoize one call for same argument

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
@@ -10,12 +11,11 @@ export const fetchPosts = () => {
   }
 };
 
-export const fetchUser = (id) => {
-  return async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({
-      type: 'FETCH_USER',
-      payload: response.data
-    });
-  }
+export const fetchUser = (id) => async dispatch => {
+  _fetchUser(id, dispatch);
 }
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+});
